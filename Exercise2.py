51fdaa212f1e620175a72ed6c03a122601071605
Zaid Khider <ZKhider@spartaglobal.com>
2019-07-15 16:39:43 +0100

    finished exercise 2 and exercise 3

---
+++ Exercise2.py
@@ -0,0 +1,17 @@
+sparta_clients = ['Goldman Sachs', 'Bo','ABN AMRO','3i Group', 'Spotify', 'Maersk', 'Channel 4','Earthport', 'Axis Capital','RBS']
+print (sparta_clients)
+print(type(sparta_clients))
+
+print(sparta_clients [0])
+
+print(f"This is a client {sparta_clients[0]}")
+print (f"{sparta_clients[1]} is a Fintech Company.")
+print (f"{sparta_clients[2]} are an oil company.")
+print (f"{sparta_clients[3]} are the biggest clients!")
+print (f"{sparta_clients[4]} make music and are a huge client!")
+print (f"{sparta_clients[5]} are a shipping company. WOW!")
+print (f"{sparta_clients[6]} is a TV channel!")
+print (f"{sparta_clients[7]} are clients.")
+print (f"{sparta_clients[8]} are an insurance company!")
+print (f"{sparta_clients[9]} is the Royal Bank of Scotland!")
+
---
+++ Exercise3.py
@@ -0,0 +1,10 @@
+story = {}
+
+story = {
+    'beginning' : "There was a graduate that joined Sparta Global.",
+    'middle':"Upon joining, he became a spartan!",
+    'end': "He finished his training worked for Goldman Sachs"
+}
+
+print(story)
+
