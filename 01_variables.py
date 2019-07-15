41b3e724320406c1dba98fa0d4252b68370de89a
Zaid Khider <ZKhider@spartaglobal.com>
2019-07-15 10:09:08 +0100

    Data type commit

---
+++ 01_variables.py
@@ -0,0 +1,14 @@
+#Variables in Python
+
+# It is like a box
+# Has a name
+# Stores things inside
+
+my_variable = 'Hello there'
+
+#variable with a string
+print (my_variable)
+
+#Reasigning a variable
+my_variable = " I'm new content being reassigned to my variable"
+print (my_variable)
