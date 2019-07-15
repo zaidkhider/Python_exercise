340d48744d0a4315a8d97e82a48d83ac1246f511
Zaid Khider <ZKhider@spartaglobal.com>
2019-07-15 13:53:22 +0100

    exercise to test our knowledge

---
+++ user_details_funny_exercise.py
@@ -0,0 +1,27 @@
+# This is where I will do my exercise
+# Filipe told me to pseudo code first
+
+# get user first name
+# save to variable
+#get user other thing
+#save somewhere else
+
+#do some formatting
+#output some data
+
+print ("What is your name?")
+first_name = input ()
+print ("What is your last name?")
+last_name = input()
+print ("What is the date (dd/mm/yyyy)?")
+date = input()
+print ("Reason for most important date?")
+reason = input ()
+print ("Give me a funny fact!")
+funny_fact = input ()
+print ("Who is your favourite pokemon?")
+favourite_pokemon = input()
+
+print (f"Welcome {first_name} {last_name}, your favourite date is on the {date} because {reason}. Your funny fact is:{funny_fact} and your favourite pokemon is {favourite_pokemon}.")
+
+
